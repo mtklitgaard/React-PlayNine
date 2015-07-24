a4e499c5f9a00aec85f5553d34ea62dffd9e724a
Dr. Klitty <mklittgaard@1800contacts.com>
2015-07-24 11:07:36 -0600

    add success messaging

--- scripts/script.jsx
+++ scripts/script.jsx
@@ -18,12 +18,36 @@ var StarsFrame = React.createClass({
 
 var ButtonFrame = React.createClass({
 	render: function  () {
-		var disabled = this.props.selectedNumbers.length === 0;
+		var disabled;
+		var correct = this.props.correct;
+		var button;
+		switch(correct){
+			case true:
+				button = (
+					<button className="btn btn-success btn-lg" >
+						<span className="glyphicon glyphicon-ok"></span>
+					</button>
+				);
+				break;
+			case false:
+				button = (
+					<button className="btn btn-danger btn-lg" >
+						<span className="glyphicon glyphicon-remove"></span>
+					</button>
+				);
+				break;
+			default:
+				disabled =  this.props.selectedNumbers.length === 0;
+				button = (
+					<button className="btn btn-primary btn-lg" disabled={disabled} onClick={this.props.checkAnswer}>
+						=
+					</button>
+				);
+		}
+
 		return (
 			<div id="button-frame">
-				<button className="btn btn-primary btn-lg" disabled={disabled}>
-				=
-				</button>
+				{button}
 			</div>
 		);
 	}
@@ -78,7 +102,8 @@ var Game = React.createClass({
 	getInitialState: function() {
 		return {
 			numberOfStars: Math.floor(Math.random()*9) + 1,
-			selectedNumbers: []};
+			selectedNumbers: [],
+			correct: null};
 	},
 	selectNumber: function(clickedNumber) {
 		if(this.state.selectedNumbers.indexOf(clickedNumber) < 0) {
@@ -95,16 +120,26 @@ var Game = React.createClass({
 
 		this.setState({selectedNumbers : selectedNumbers});
 	},
+	checkAnswer: function() {
+		var correct = this.state.numberOfStars === this.sumOfSelectedNumbers();
+		this.setState({correct: correct});
+	},
+	sumOfSelectedNumbers: function() {
+		return this.state.selectedNumbers.reduce(function(p,n) {
+			return p+n;
+		}, 0)
+	},
 	render: function () {
 		var selectedNumbers = this.state.selectedNumbers;
 		var numberOfStars = this.state.numberOfStars;
+		var correct = this.state.correct;
 		return (
 			<div id="game">
 				<h2>Play Nine</h2>
 				<hr />
 				<div className="clearfix">
 					<StarsFrame  numberOfStars= {numberOfStars}/>
-					<ButtonFrame selectedNumbers={selectedNumbers}/>
+					<ButtonFrame selectedNumbers={selectedNumbers} correct={correct} checkAnswer={this.checkAnswer}/>
 					<AnswerFrame selectedNumbers={selectedNumbers} unselectNumber={this.unselectNumber} />
 				</div>
 
