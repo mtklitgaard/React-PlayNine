2119ec9910bf96b5aee10d0963560c596251076f
Dr. Klitty <mklittgaard@1800contacts.com>
2015-07-27 20:34:03 -0600

    start using webpack

--- scripts/script.jsx
+++ scripts/script.jsx
@@ -1,121 +1,12 @@
-var StarsFrame = React.createClass({
-	render: function () {
-		var stars = [];
-		for(var i = 0; i<this.props.numberOfStars; i++){
-			stars.push(
-					<span className="glyphicon glyphicon-star"></span>
-				);
-		}
-		return (
-			<div id="stars-frame">
-				<div className="well">
-					{stars}
-				</div>
-			</div>
-		);
-	}
-});
+var StarsFrame = require('starts.jsx');
 
-var ButtonFrame = React.createClass({
-	render: function  () {
-		var disabled;
-		var correct = this.props.correct;
-		var button;
-		switch(correct){
-			case true:
-				button = (
-					<button className="btn btn-success btn-lg" onClick={this.props.acceptAnswer}>
-						<span className="glyphicon glyphicon-ok"></span>
-					</button>
-				);
-				break;
-			case false:
-				button = (
-					<button className="btn btn-danger btn-lg" >
-						<span className="glyphicon glyphicon-remove"></span>
-					</button>
-				);
-				break;
-			default:
-				disabled =  this.props.selectedNumbers.length === 0;
-				button = (
-					<button className="btn btn-primary btn-lg" disabled={disabled} onClick={this.props.checkAnswer}>
-						=
-					</button>
-				);
-		}
+var ButtonFrame = require('button.jsx');
 
-		return (
-			<div id="button-frame">
-				{button}
-				<br /><br />
-				<button className="btn btn-warning btn-xs" onClick={this.props.redraw} disabled={this.props.redraws === 0}>
-					<span className="glyphicon glyphicon-refresh"></span>
-					&nbsp;
-					{this.props.redraws}
-				</button>
-			</div>
-		);
-	}
-});
+var AnswerFrame = require('answer.jsx');
 
-var AnswerFrame = React.createClass({
-	render: function() {
-		var props = this.props;
-		var selectedNumbers = props.selectedNumbers.map(function(i){
-			return (
-					<span onClick={props.unselectNumber.bind(null, i)}>
-						{i}
-					</span>
-					)
-		});
+var NumbersFrame = require('numbers.jsx');
 
-		return (
-			<div id="answer-frame">
-				<div className="well">
-					{selectedNumbers}	
-				</div>
-			</div>
-		);
-	}
-});
-
-var NumbersFrame = React.createClass({
-	render:function() {
-		var numbers = []
-		var className
-		var selectedNumbers=this.props.selectedNumbers;
-		var usedNumbers = this.props.usedNumbers;
-		var selectNumber = this.props.selectNumber;
-
-		for(var i = 1; i <= 9; i++) {
-			className = "number selected-" + (selectedNumbers.indexOf(i)>=0)
-			className += " used-" + (usedNumbers.indexOf(i)>=0);
-			numbers.push(
-					<div className={className} onClick={selectNumber.bind(null, i)}>{i}</div>
-				);
-		}
-
-		return (
-			<div id="numbers-frame">
-				<div className="well">
-					{numbers}
-				</div>	
-			</div>
-		);
-	}
-});
-
-var DoneFrame = React.createClass({
-	render: function() {
-		return (
-				<div className="well text-center">
-					<h2>{this.props.doneStatus}</h2>
-					<button className="btn btn-default" onClick={this.props.resetGame}>Play Again</button>
-				</div>
-			);
-	}
-});
+var DoneFrame = require('done.jsx');
 
 var Game = React.createClass({
 	getInitialState: function() {
